6d2226f8e19cea9f5e88aed4b0a38e2a92632881
CAPAlencar <cezarseven77@gmail.com>
2022-09-21 16:16:45 -0400

    add do javaScrip e animacoes

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// ativar links do Menu
+
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+    if (url.includes(href)) {
+        link.classList.add('ativo');
+    }
+}
+
+links.forEach(ativarLink);
+
+// ativar intens do orcamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento){
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
+
+
+// perguntas ativas
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    console.log(ativa);
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+    
+/// galeria de Bicicletas
+
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width:900px)").matches;
+    if (media){
+        galeriaContainer.prepend(img);
+    } 
+}
+        
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+////// ANIMACAO
+    
+if (window.SimpleAnime){
+new SimpleAnime();
+}
+
